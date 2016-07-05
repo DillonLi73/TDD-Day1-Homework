79d7c0174ead0f467f3164586f36e31038962bc1
dillon <dillonli0703@gmail.com>
2016-07-05 15:39:39 +0800

    測試用 order 集合改用 function 取得，避免跑測試時被更動資料

--- TDD-Day1-HomeworkTests/OrderCalculatorTests.cs
+++ TDD-Day1-HomeworkTests/OrderCalculatorTests.cs
@@ -11,27 +11,30 @@ namespace TDD_Day1_Homework.Tests
     [TestClass()]
     public class OrderCalculatorTests
     {
-        private List<Order> _orderData1 = new List<Order> {
-            new Order(1, 1, 11, 21),
-            new Order(2, 2, 12, 22),
-            new Order(3, 3, 13, 23),
-            new Order(4, 4, 14, 24),
-            new Order(5, 5, 15, 25),
-            new Order(6, 6, 16, 26),
-            new Order(7, 7, 17, 27),
-            new Order(8, 8, 18, 28),
-            new Order(9, 9, 19, 29),
-            new Order(10, 10, 20, 30),
-            new Order(11, 11, 21, 31)
-        };
+        private List<Order> GetOrders()
+        {
+            return new List<Order> {
+                new Order(1, 1, 11, 21),
+                new Order(2, 2, 12, 22),
+                new Order(3, 3, 13, 23),
+                new Order(4, 4, 14, 24),
+                new Order(5, 5, 15, 25),
+                new Order(6, 6, 16, 26),
+                new Order(7, 7, 17, 27),
+                new Order(8, 8, 18, 28),
+                new Order(9, 9, 19, 29),
+                new Order(10, 10, 20, 30),
+                new Order(11, 11, 21, 31)
+            };
+        }
 
         [TestMethod()]
-        public void SumOneFieldByPagingTest_orders_is_orderData1_fieldName_is_Cost_numPerPage_is_3_result_should_be_6_15_24_21()
+        public void SumOneFieldByPagingTest_orders_is_GetOrders_fieldName_is_Cost_numPerPage_is_3_result_should_be_6_15_24_21()
         {
             // arrange
             OrderCalculator target = new OrderCalculator();
 
-            List<Order> orders = this._orderData1;
+            List<Order> orders = this.GetOrders();
             string fieldName = "Cost";
             int numPerPage = 3;
             List<int> excepted = new List<int> { 6, 15, 24, 21 };
@@ -44,11 +47,11 @@ namespace TDD_Day1_Homework.Tests
         }
 
         [TestMethod()]
-        public void SumOneFieldByPagingTest_orders_is_orderData1_fieldName_is_Revenue_numPerPage_is_4_result_should_be_50_66_60()
+        public void SumOneFieldByPagingTest_orders_is_GetOrders_fieldName_is_Revenue_numPerPage_is_4_result_should_be_50_66_60()
         {
             // arrange
             OrderCalculator target = new OrderCalculator();
-            List<Order> orders = this._orderData1;
+            List<Order> orders = this.GetOrders();
             string fieldName = "Revenue";
             int numPerPage = 4;
             List<int> excepted = new List<int> { 50, 66, 60 };
