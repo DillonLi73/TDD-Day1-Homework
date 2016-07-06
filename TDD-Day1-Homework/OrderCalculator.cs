382fe28876649ada95c70fc853c4785334b29637
dillon <dillonli0703@gmail.com>
2016-07-06 12:54:40 +0800

    依據測試紅燈綠燈補完 production code

--- TDD-Day1-Homework/OrderCalculator.cs
+++ TDD-Day1-Homework/OrderCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,29 @@ namespace TDD_Day1_Homework
     {
         public List<int> SumOneFieldByPaging(List<Order> orders, string fieldName, int numPerPage)
         {
-            throw new NotImplementedException();
+            PropertyInfo prop = typeof(Order).GetProperty(fieldName);
+            if ( prop == null || prop.PropertyType != typeof(Int32) )
+            {
+                throw new TypeAccessException("the type of field " + fieldName + " is not Int32!");
+            }
+
+            List<int> result = new List<int>();
+            int processedNum = 0;
+            while (processedNum < orders.Count)
+            {
+                IEnumerable<Order> orderPage = orders.Skip(processedNum).Take(numPerPage);
+
+                int fieldSum = 0;
+                foreach (Order order in orderPage)
+                {
+                    fieldSum += (int)prop.GetValue(order);
+                }
+
+                result.Add(fieldSum);
+                processedNum += numPerPage;
+            }
+
+            return result;
         }
     }
 }
